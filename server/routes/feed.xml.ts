4d92e14733958dce7f177d8b02df29bd552f88c6
OOTORIKOTORI <s0a1t0o8shi@gmail.com>
2025-10-11 17:44:23 +0900

    fix(routes): revert sitemap/feed to fs readFile (avoid ?raw issue in Nitro server build)

--- server/routes/feed.xml.ts
+++ server/routes/feed.xml.ts
@@ -1,5 +1,11 @@
 import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const here = dirname(fileURLToPath(import.meta.url))
+const feedPath = join(here, '../../public/feed.xml')
+
 export default defineEventHandler(async e => {
   setHeader(e, 'Content-Type', 'application/xml; charset=utf-8')
-  return await readFile('./public/feed.xml', 'utf8')
+  return await readFile(feedPath, 'utf8')
 })
--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,5 +1,11 @@
 import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const here = dirname(fileURLToPath(import.meta.url))
+const sitemapPath = join(here, '../../public/sitemap.xml')
+
 export default defineEventHandler(async e => {
   setHeader(e, 'Content-Type', 'application/xml; charset=utf-8')
-  return await readFile('./public/sitemap.xml', 'utf8')
+  return await readFile(sitemapPath, 'utf8')
 })
